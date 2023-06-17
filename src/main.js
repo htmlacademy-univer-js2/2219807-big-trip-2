343129c61d559888b4b16e95367fc449583943e8
mihail323i21 <mihail323i21@gmail.com>
2023-06-17 22:04:23 +0500

    filter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import TripPresenter from './presenter/trip-presenter';
 import PointModel from './model/point-model';
-import {generateFilter} from './mock/filter';
-import {render} from './framework/render';
-import FiltersView from './view/filters-view';
 import FilterModel from './model/filter-model';
 import FilterPresenter from './presenter/filter-presenter';
 
@@ -14,7 +11,6 @@ const filterModel = new FilterModel();
 
 const pointsModel = new PointModel();
 pointsModel.init();
-const filters = generateFilter(pointsModel.points);
 
 const filterPresenter = new FilterPresenter(siteHeaderElement.querySelector('.trip-controls__filters'), filterModel, pointsModel);
 filterPresenter.init();
@@ -22,4 +18,3 @@ filterPresenter.init();
 const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'), pointsModel, FilterModel);
 tripPresenter.init();
 
-render(new FiltersView(filters), siteHeaderElement.querySelector('.trip-controls__filters'));
